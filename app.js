074a267483323e6e24b26d04d11ae586ec0fc9c0
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-04-08 20:36:15 -0300

    JSON curto

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ let pi = 3.14;
 function soma(a, b = 1){
     console.log(a + b);
 }
+
+//JSON encurtado
+
+var user1 = {
+    nome: nome,
+    sobrenome: sobrenome,
+    pi: pi
+};
+
+var user2 = {
+    nome,
+    sobrenome,
+    pi
+}
+
+console.log(user1);
+console.log(user2);
